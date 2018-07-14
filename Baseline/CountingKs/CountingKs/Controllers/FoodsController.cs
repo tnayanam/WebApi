865887bcab1ec8d0dd22a129c2f69e5bb1052b6d
tnayanam <nayatanuj@gmail.com>
2018-07-14 11:44:13 -0400

    Created a basic API Controller

---
+++ Baseline/CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+// so for creating this api controller, I selected from controllers dropdown
+// empty api controller, I did not select the empty asp.net mvc controller.
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+    }
+}
