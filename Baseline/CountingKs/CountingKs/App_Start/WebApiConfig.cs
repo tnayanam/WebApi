b6ac47f5770b3116b9646f3675183379650b2f82
tnayanam <nayatanuj@gmail.com>
2018-07-14 13:44:06 -0400

    first api method, TAKE linq, OrderBy LINQ

--- Baseline/CountingKs/CountingKs/App_Start/WebApiConfig.cs
+++ Baseline/CountingKs/CountingKs/App_Start/WebApiConfig.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace CountingKs
 {
-  public static class WebApiConfig
-  {
-    public static void Register(HttpConfiguration config)
+    public static class WebApiConfig
     {
-      config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
-      );
-
-      // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
-      // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
-      // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
-      //config.EnableQuerySupport();
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            //           GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+            //.Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+            //                              "text/html",
+            //                              StringComparison.InvariantCultureIgnoreCase,
+            //                              true,
+            //                              "application/json"));
+            //config.EnableSystemDiagnosticsTracing();
+            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
+            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
+            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
+            //config.EnableQuerySupport();
+        }
     }
-  }
 }
--- Baseline/CountingKs/CountingKs/Controllers/FoodsController.cs
+++ Baseline/CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -1,9 +1,21 @@
-﻿using System.Web.Http;
+﻿using CountingKs.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
 // so for creating this api controller, I selected from controllers dropdown
 // empty api controller, I did not select the empty asp.net mvc controller.
 namespace CountingKs.Controllers
 {
     public class FoodsController : ApiController
     {
+        public List<Data.Entities.Food> Get()
+        {
+            var repo = new CountingKsRepository(new CountingKsContext());
+            var results = repo.GetAllFoods()
+                .OrderBy(f => f.Description)
+                .Take(25)
+                .ToList();
+            return results;
+        }
     }
 }
