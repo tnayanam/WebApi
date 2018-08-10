c8fa138b7e697ece933644fc591ae69d7b8d2668
unknown <tnayanam@syr.edu>
2018-08-10 15:45:38 -0400

    xml as result as content negotiation

---
+++ HelloWebApiDemo/HelloWebApiDemo/SecondController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HelloWebApiDemo
+{
+    public class SecondController: ApiController
+    {
+        public string Get()
+        {
+            return "my";
+        }
+        // if you want the resrult in xmll check the content negotiation.
+    }
+}
