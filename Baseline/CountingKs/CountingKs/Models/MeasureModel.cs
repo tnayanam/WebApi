f4715707f5b44abd31b7c8ae1e04d6a59f0bf1ef
unknown <tnayanam@syr.edu>
2018-08-10 15:35:37 -0400

    started a new project on ASP.Net Web API

--- Baseline/CountingKs/CountingKs/Models/MeasureModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CountingKs.Models
-{
-    public class MeasureModel
-    {
-        public string Url { get; set; }
-        public string Description { get; set; }
-        public double Calories { get; set; }
-    }
-}
