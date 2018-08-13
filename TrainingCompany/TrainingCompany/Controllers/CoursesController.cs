a2be0af9c4cc8aaf8e1ffb1d5938a0d05b328d23
unknown <tnayanam@syr.edu>
2018-08-13 15:56:35 -0400

    Important Ajax Post Request from MVC to API Controller Routing stuff

---
+++ TrainingCompany/TrainingCompany/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TrainingCompany.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        public IEnumerable<course> Get()
+        {
+            return courses;
+        }
+
+        static List<course> courses = InitCourses();
+        private static List<course> InitCourses()
+        {
+            var ret = new List<course>();
+            ret.Add(new course{id = 0, title = "Web Application1"});
+            ret.Add(new course{id = 1, title = "Web Application2"});
+            return ret;
+        }
+
+
+    }
+
+    public class course
+    {
+        public int id;
+        public string title;
+    }
+}
