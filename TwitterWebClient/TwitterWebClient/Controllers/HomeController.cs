7362e5fb96242363bffc6070d43fe75bc18c933c
unknown <tnayanam@syr.edu>
2018-08-14 13:34:16 -0400

    new solution HttpClient added

---
+++ TwitterWebClient/TwitterWebClient/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace TwitterWebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Tweets model = null;
+            var client = new HttpClient();
+            var task = 
+                client.GetAsync(
+                "http://search.twitter.com/search.json?q=pluralsight")
+                .ContinueWith((taskwithresponse) =>
+                {
+                    var response = taskwithresponse.Result;
+                    var readtask = response.Content.ReadAsAsync<Tweets>();
+                    readtask.Wait();
+                    model = readtask.Result;
+                });
+            task.Wait();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TwitterWebClient/TwitterWebClient/Controllers/Tweets.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitterWebClient.Controllers
+{
+    public class Tweets
+    {
+        public Tweet[] resuls;
+    }
+    public class Tweet
+    {
+        [JsonProperty("from_user")]
+        public string UserName { get; set; }
+        public string TweetText { get; set; }
+    }
+
+}
+// if you are trying to make an api call and you want the data name to be different from the
+// one that is coming from reponse then you need to apply jsonproperty in them.
+// check the screenshot
