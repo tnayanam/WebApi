d98b4be113a741ffcf7f944b58858c910b222178
Tanuj Nayanam <tnayanam@sessionm.lan>
2018-08-28 11:22:35 -0400

    Random

---
+++ CallAPI/CallAPI/CallAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CallAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string createText = "Hello and Welcome" + Environment.NewLine;
+            File.WriteAllText(@"C:\Users\lenovo\Documents\VisualStudio 2015\Projects\WebApplication11\foo.txt", createText);
+            Task.Run(() => CallHttp());
+            Task.WaitAll();
+            Task.WaitAny();
+            Thread.Sleep(1000);
+            //System.Console.ReadLine();
+        }
+        // Simple async function returning a string...
+
+        static public async void CallHttp()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.crowdin.com/");
+            HttpResponseMessage response = await client.GetAsync("api/project/referville/download/all.zip?key=f23e2c9181ef6630804761d33c0de737");
+            using (Stream stream = await response.Content.ReadAsStreamAsync())
+            using (FileStream fs = new FileStream(@"C:\temp\alpha\test.zip", FileMode.Create))
+                await stream.CopyToAsync(fs);
+            System.IO.Compression.ZipFile.ExtractToDirectory(@"C:\temp\alpha\test.zip", @"C:\temp\beta");
+        }
+    }
+}
