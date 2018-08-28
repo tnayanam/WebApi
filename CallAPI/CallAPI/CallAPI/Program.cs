493bfd9dd865632cfc51305677f523868b4f301f
Tanuj Nayanam <tnayanam@sessionm.lan>
2018-08-28 11:24:20 -0400

    Make a call to third party API

--- CallAPI/CallAPI/CallAPI/Program.cs
+++ CallAPI/CallAPI/CallAPI/Program.cs
@@ -19,15 +19,15 @@ namespace CallAPI
             Task.WaitAll();
             Task.WaitAny();
             Thread.Sleep(1000);
-            //System.Console.ReadLine();
         }
         // Simple async function returning a string...
-
+        // this is how you call a third party API from C#
         static public async void CallHttp()
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri("https://api.crowdin.com/");
-            HttpResponseMessage response = await client.GetAsync("api/project/referville/download/all.zip?key=f23e2c9181ef6630804761d33c0de737");
+            HttpResponseMessage response = await client.GetAsync("api/project/referville/download/all.zip?key=f23e2c9181ef6630804761d33c0de737"); // see here we have project name and the secret key
+            // which is needed to make the call.
             using (Stream stream = await response.Content.ReadAsStreamAsync())
             using (FileStream fs = new FileStream(@"C:\temp\alpha\test.zip", FileMode.Create))
                 await stream.CopyToAsync(fs);
